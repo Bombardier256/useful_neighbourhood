a37f0b453e66b946ec55281c0295802111156b90
Bombardier256 <4477617@gmail.com>
2025-03-20 16:28:41 +0200

    castomize neighbour and category in admin.py

--- lend_assist/admin.py
+++ lend_assist/admin.py
@@ -1,9 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from lend_assist.models import Category, Service
 from lend_assist.models import Neighbour, Request
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name",]
+
+
+@admin.register(Neighbour)
+class NeighbourAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("phone", "address",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("address", "phone", "service",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "address",
+                        "phone",
+                        "service",
+                    )
+                },
+            ),
+        )
+    )
+
 admin.site.register(Service)
-admin.site.register(Neighbour)
 admin.site.register(Request)
--- lend_assist/models.py
+++ lend_assist/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
 
+    def __str__(self):
+        return self.name
+
 
 class Service(models.Model):
     name = models.CharField(max_length=255)
