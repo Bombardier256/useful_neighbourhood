740e533d212db9ef5fefd86ded6ade7bbaffd0a6
Bombardier256 <4477617@gmail.com>
2025-03-20 14:54:08 +0200

    Add index function to views

--- lend_assist/views.py
+++ lend_assist/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from lend_assist.models import Category, Service
+from lend_assist.models import Neighbour, Request
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_users = Neighbour.objects.count()
+    num_services = Service.objects.count()
+    num_requests = Request.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_users": num_users,
+        "num_services": num_services,
+        "num_requests": num_requests,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "lend_assist/index.html", context=context)
+
