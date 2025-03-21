7dbcc58293b965874aea293d21f83bae7963ebe3
Bombardier256 <4477617@gmail.com>
2025-03-21 17:19:48 +0200

    Remove deleted field of neighbour from RequestAdmin

--- lend_assist/admin.py
+++ lend_assist/admin.py
@@ -37,7 +37,7 @@ class NeighbourAdmin(UserAdmin):
 @admin.register(Request)
 class RequestAdmin(admin.ModelAdmin):
     list_display = [
-        "name", "date", "complete", "category", "neighbour", "reward", "description",
+        "name", "date", "complete", "category", "reward", "description",
     ]
     search_fields = ["name", "description"]
     list_filter = ["category", "date"]
