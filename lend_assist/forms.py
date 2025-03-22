249c606862f388a7055b2e8c75187552ce5e2588
Bombardier256 <4477617@gmail.com>
2025-03-22 15:55:20 +0200

    Add password management to NeighbourCreateForm

--- lend_assist/forms.py
+++ lend_assist/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from django.core.exceptions import ValidationError
 
 from lend_assist.models import (
@@ -10,9 +11,28 @@ from lend_assist.models import (
 
 
 class NeighbourCreateForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = Neighbour
-        fields = ("username", "first_name", "last_name", "phone", "address", "email")
+        fields = ("username", "password", "first_name", "last_name", "phone", "address", "email",)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Passwords do not match!")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.password = make_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
 
 
 class NeighbourUpdateForm(forms.ModelForm):
