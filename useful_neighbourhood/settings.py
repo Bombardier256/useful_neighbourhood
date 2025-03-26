c1000b6641b3eec192c74bb83d889fe85a6273db
Bombardier256 <4477617@gmail.com>
2025-03-26 20:20:22 +0200

    Preseted frontend 1 version

--- useful_neighbourhood/settings.py
+++ useful_neighbourhood/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "lend_assist.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -127,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+ASSETS_ROOT = "/static/assets"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
