5178f41281f488eabdd5eaed8ae2e6e20aaee0fe
Bombardier256 <4477617@gmail.com>
2025-03-28 13:26:50 +0200

    Update reedme.md

--- useful_neighbourhood/settings.py
+++ useful_neighbourhood/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1abhfv7rj0n*#lwwqu_x5a9il$tz2q5m4(6%soyheomm7nb+na"
+
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
